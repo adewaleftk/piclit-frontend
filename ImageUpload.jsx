6ff1b8268c98b606a1f7c562cf3b223ec3d73ae7
Adewale Fatoki <adewaleftk@gmail.com>
2023-08-30 23:40:56 +0100

    frontend updated

--- ImageUpload.jsx
+++ ImageUpload.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 
 function ImageUpload() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [compressedImage, setCompressedImage] = useState(null);
 
   function handleFileChange(event) {
     setSelectedFile(event.target.files[0]);
+    setCompressedImage(null); // Clear the compressed image when a new file is selected
   }
 
   async function handleUpload() {
@@ -13,22 +15,43 @@ function ImageUpload() {
     const formData = new FormData();
     formData.append('image', selectedFile);
 
-    const headers = {
-      'Content-Type': 'multipart/form-data',
-    };
-
     try {
       const response = await fetch('https://piclit-backend.onrender.com/api/compress', {
         method: 'POST',
         body: formData,
-        headers,
       });
-      const data = await response.json();
 
-      if (response.ok) console.log(data.message);
-      else console.error('Image compression failed');
+      // Check if the response status is 200 (OK)
+      if (response.status === 200) {
+        // Get the response body as a Blob
+        const compressedBlob = await response.blob();
+
+        // Create an object URL for the Blob
+        const compressedImageUrl = URL.createObjectURL(compressedBlob);
+
+        // Set the compressed image URL
+        setCompressedImage(compressedImageUrl);
+      } else {
+        // Display an error message
+        console.error('Image compression failed');
+      }
     } catch (error) {
       console.error('Network error:', error);
+    }
+  }
+
+  function handleDownload() {
+    if (compressedImage) {
+      // Create a temporary anchor element
+      const downloadLink = document.createElement('a');
+      downloadLink.href = compressedImage;
+      downloadLink.download = 'compressed-image.jpg'; // Set the desired file name
+
+      // Simulate a click on the anchor element
+      downloadLink.click();
+
+      // Clean up the anchor element
+      URL.revokeObjectURL(compressedImage);
     }
   }
 
@@ -36,6 +59,12 @@ function ImageUpload() {
     <div>
       <input type="file" accept="image/*" onChange={handleFileChange} />
       <button onClick={handleUpload}>Compress Image</button>
+      {compressedImage && (
+        <div>
+          <img src={compressedImage} alt="Compressed" />
+          <button onClick={handleDownload}>Download Compressed Image</button>
+        </div>
+      )}
     </div>
   );
 }
