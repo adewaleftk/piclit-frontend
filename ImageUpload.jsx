524a25e2759f6d7eb649a0991bb7f4d5956648f1
Adewale Fatoki <adewaleftk@gmail.com>
2023-08-30 03:47:24 +0100

    pivlit

---
+++ ImageUpload.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+function ImageUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  function handleFileChange(event) {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  async function handleUpload() {
+    if (!selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+
+    try {
+      const response = await fetch('https://piclit-backend.onrender.com/api/compress', { method: 'POST', body: formData });
+      const data = await response.json();
+
+      if (response.ok) console.log(data.message);
+      else console.error('Image compression failed');
+    } catch (error) {
+      console.error('Network error:', error);
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Compress Image</button>
+    </div>
+  );
+}
+
+export default ImageUpload;
